45b22da75b6024aede1df1567bca876ee72d2918
Danil <danil-danil-korolev@bk.ru>
2023-06-14 20:36:11 +0700

    fix vercel 4

--- apps/server/src/utils/enums.ts
+++ apps/server/src/utils/enums.ts
@@ -1,7 +1,8 @@
 import { ValueOf } from "utils/types"
 
 export const DirectoryPaths = {
-  PublicClientImages: "/client/public/images/"
+  // PublicClientImages: "/client/public/images/"
+  PublicClientImages: "/images/"
 } as const
 
 export type DirectoryPaths = ValueOf<typeof DirectoryPaths> 
